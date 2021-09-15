b5db785b9c9795a609692986736d9cd3e2ca9ef4
mii4a <beppu0918@outlook.jp>
2021-09-15 23:23:32 +0900

    Add redux #2

--- frontend/web-front/.next/static/chunks/webpack.js
+++ frontend/web-front/.next/static/chunks/webpack.js
@@ -171,7 +171,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a121cf0ff335758feef4"; }
+/******/ 		__webpack_require__.h = function() { return "ecbf447e1f48c5c055ec"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
