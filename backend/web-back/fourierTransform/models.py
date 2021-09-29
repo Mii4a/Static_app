cf0629452a1320212d17f5dac7b7f80f0ec51631
mii4a <beppu0918@outlook.jp>
2021-09-30 00:34:00 +0900

    write create_date, title, description to FFT #3

--- backend/web-back/fourierTransform/models.py
+++ backend/web-back/fourierTransform/models.py
@@ -1,3 +1,10 @@
+import numpy as np
 from django.db import models
 
+
 # Create your models here.
+class FFT(models.Model):
+  create_date = models.DateTimeField(auto_now_add=true)
+  
+  title = models.CharField(max_length=100, null=True, blank=True)
+  description = models.TextField(null=True, blank=True)
